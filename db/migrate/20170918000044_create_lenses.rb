d24fd2caa4531e8e6e82e5b60240186691e8cc1f
Ben Rudolph <rudolphben@gmail.com>
2017-09-17 17:05:12 -0700

    slow seed

--- db/migrate/20170918000044_create_lenses.rb
+++ db/migrate/20170918000044_create_lenses.rb
@@ -1,7 +1,7 @@
 class CreateLenses < ActiveRecord::Migration[5.1]
   def change
     create_table :lenses do |t|
-      t.string :lens_id, index: true
+      t.string :lens_id, index: true, unique: true
       t.string :image_url
       t.string :name
       t.text :description
